2e84daa25505852325314910dab84a8967a3cd36
Yong Liang <yongl9460@gmail.com>
2019-03-25 12:17:31 +1100

    changed code to export styled components

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
-export { default as Tabs } from './components/Tabs';
-export { default as TabList } from './components/TabList';
-export { default as Tab } from './components/Tab';
-export { default as TabPanel } from './components/TabPanel';
+// export { default as Tabs } from './components/Tabs';
+// export { default as TabList } from './components/TabList';
+// export { default as Tab } from './components/Tab';
+// export { default as TabPanel } from './components/TabPanel';
 export { reset as resetIdCounter } from './helpers/uuid';
+// export { default as NavTab } from './components/NavTab.style';
+// export { default as ExampleApp } from './examples/StyledComponents.style';
+
+// export { Tabs } from './examples/StyledComponents.style';
+// export { TabList } from './examples/StyledComponents.style';
+// export { Tab } from './examples/StyledComponents.style';
+// export { TabPanel } from './examples/StyledComponents.style';
+
+export { Tabs } from './components/NavTab.style';
+export { TabList } from './components/NavTab.style';
+export { Tab } from './components/NavTab.style';
+export { TabPanel } from './components/NavTab.style';
